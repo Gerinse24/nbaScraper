c4d9e41f048facd59e128cd9f8189d9dbae15c32
G.A.Shaw <geronimo.shaw@gmail.com>
2019-10-15 10:05:40 +0800

    Update nbaScraper.py
    Comment update for better description.

--- nbaScraper.py
+++ nbaScraper.py
@@ -13,11 +13,11 @@ soup = bs4.BeautifulSoup(html, "lxml")
 url.close()
 total = []
 teams = []
-for tag in soup.find_all("td", {"class":"final-score"}):    # Finds all scores from Games. Saves them in total.
+for tag in soup.find_all("td", {"class":"final-score"}):    # Finds all scores from todays games and stores them in total.
     total.append(tag.text)
 
-for team in soup.find_all("td", {"class":"team-abbrv"}):    # Finds all team abbreviations. Saves them in teams.
+for team in soup.find_all("td", {"class":"team-abbrv"}):    # Finds team abbreviations from todays games and stores them in teams.
     teams.append(team.text)
 
-for i in range(len(total)):     # Iterates through one list but combines them into one string.
+for i in range(len(total)):     # Iterates over the len of total which will be the same as the amount of teams, and prints them.
     print(teams[i] + ' ' + total[i])
